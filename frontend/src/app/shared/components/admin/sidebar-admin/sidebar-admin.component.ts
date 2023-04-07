8c34db194a92038ebb59c0208c04061dbd31e673
EL AYACHI Abdelmajid <Abdelmajid.ELAYACHI@cegedim.com>
2023-04-07 18:00:18 +0200

    add admin dashboard

---
+++ frontend/src/app/shared/components/admin/sidebar-admin/sidebar-admin.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sidebar-admin',
+  templateUrl: './sidebar-admin.component.html',
+  styleUrls: ['./sidebar-admin.component.css']
+})
+export class SidebarAdminComponent {
+
+  showSidebar = false;
+
+  openCloseSidebar() {
+    this.showSidebar = !this.showSidebar;
+  }
+}
